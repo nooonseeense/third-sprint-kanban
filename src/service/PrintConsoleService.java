95ff8a0235c0515fe30af267db66c8fc24e7e46c
nooonseeense <egor-bolt12@yandex.ru>
2022-07-17 08:41:39 +0300

    feat: добавить новый метод printSubTasksInEpic

--- src/service/PrintConsoleService.java
+++ src/service/PrintConsoleService.java
@@ -1,22 +1,37 @@
 package service;
 import tasks.Epic;
+import tasks.Subtask;
 import tasks.Task;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrintConsoleService {
+    private final String REPEATED = new String(new char[120]).replace("\0", "=");
 // 3.1 Получение списка всех задач мапы task
     public void printTasks(List<Task> tasks) {
-        System.out.println("Таски:");
+        System.out.println(REPEATED + "\nТаски:");
         for (Task values : tasks) {
             System.out.println(values);
         }
     }
 
     public void printEpics(List<Epic> epics) {
-        System.out.println("Эпики:");
+        System.out.println(REPEATED + "\nЭпики:");
         for (Epic epic : epics) {
             System.out.println("У эпика " + epic + ", подзадача: " + epic.getSubtaskIds());
         }
+        System.out.println(REPEATED);
+    }
+
+    public void printSubTasksInEpic(Epic epic, List<Subtask> subtask) {
+        ArrayList<Subtask> values = new ArrayList<>();
+
+        for (Subtask value : subtask) {
+            if (value.getEpicId() == epic.getId()) {
+                values.add(value);
+            }
+        }
+        System.out.println("У эпика " + epic + ", подзадачи: " + values);
     }
 }
