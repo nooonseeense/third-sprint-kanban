6e8e9e3a5f0e0fe6714914b62d4db21463d62a36
nooonseeense <egor-bolt12@yandex.ru>
2022-08-22 07:41:45 +0300

    fix: сделать метод removeNode(CustomNode<Task> node) с корректной логикой изменения ссылок

--- src/util/CustomLinkedList.java
+++ src/util/CustomLinkedList.java
@@ -25,22 +25,23 @@ public class CustomLinkedList<Task> implements CustomList<Task> {
         if (node == null) {
             return;
         }
-        if (node.prev == null) {
-            head = node.next;
-        }
-        if (node.next == null) {
-            tail = node.prev;
-        }
-        if (node.prev != null && node.next != null) {
-            CustomNode<Task> left = node.prev;
-            CustomNode<Task> right = node.next;
 
-            left.next = right;
-            right.prev = left;
+        final CustomNode<Task> prev = node.prev; // ССЫЛКА на предыдущую ноду
+        final CustomNode<Task> next = node.next; // ССЫЛКА на следующую ноду
 
-            node.next = null;
-            node.prev = null;
+        if (prev == null) { // ЕСЛИ предыдущий узел == null, ТО головой списка становится СЛЕДУЮЩИЙ узел
+            head = next;
+        } else { // ЕСЛИ узел находится в центре списка, ТО:
+            prev.next = next; // ТО поле NEXT у предыдущей ноды, начинает ссылаться на поле NEXT удаляемой
+            node.prev = null; // поле PREV у удаляемой ноды зануляем, т.к мы изменили ссылки и сохранили связь
         }
+        if (next == null) { // ЕСЛИ следующий узел == null, то хвостом списка становится ПРЕДЫДУЩИЙ узел
+            tail = prev;
+        } else { // ЕСЛИ узел находится в центре списка, ТО:
+            next.prev = prev; // ТО поле PREV у следующей ноды, начинает ссылаться на поле PREV удаляемой
+            node.next = null; // поле NEXT у удаляемой ноды зануляем, т.к мы изменили ссылки и сохранили связь
+        }
+        node.task = null; // Зануляем значение узла
     }
 
     @Override
