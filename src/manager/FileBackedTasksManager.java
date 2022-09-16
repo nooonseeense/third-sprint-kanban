db4100c7873fef70fee3ccfa48b4e0fd279d0045
nooonseeense <egor-bolt12@yandex.ru>
2022-09-16 09:03:33 +0300

    feat: убрать параметры в методе save() и изменить алгоритм

--- src/manager/FileBackedTasksManager.java
+++ src/manager/FileBackedTasksManager.java
@@ -5,13 +5,11 @@ import exceptions.ManagerSaveException;
 import tasks.Epic;
 import tasks.Subtask;
 import tasks.Task;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.HashMap;
 
 public class FileBackedTasksManager extends InMemoryTasksManager implements TasksManager {
     private final File file;
@@ -29,51 +27,69 @@ public class FileBackedTasksManager extends InMemoryTasksManager implements Task
 
         Task task1 = new Task("Задача[1]", "Описание[Таск]", Status.NEW);
         fileBackedTasksManager.addTask(task1);
+        fileBackedTasksManager.getTaskById(task1.getId());
 
         Epic epic1 = new Epic("Епик[1]", "Описание[Епик]");
         fileBackedTasksManager.addEpic(epic1);
+        fileBackedTasksManager.getEpicById(epic1.getId());
 
         Subtask subtask1 = new Subtask("Подзадача[1]", "Описание[Саб]", Status.DONE, epic1.getId());
         fileBackedTasksManager.addSubTask(subtask1);
-
-        //fileBackedTasksManager.getEpicById(task1.getId());
-
+        fileBackedTasksManager.getSubtaskById(subtask1.getId());
     }
 
     /**
      * Сохраняет текущее состояние менеджера в указанный файл при вызове методов добавления
      */
-    private <T extends Task> void save(T task) {
+    private void save() {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
-            addTasksToFile(bufferedWriter, task);
+            bufferedWriter.write("id,type,name,status,description,epic\n");
+
+            addTasksToFile(bufferedWriter, tasks.values());
+            addTasksToFile(bufferedWriter, epics.values());
+            addTasksToFile(bufferedWriter, subtasks.values());
+
+            bufferedWriter.write(
+                    "\n"
+                    + historyToString(historyManager)
+                    + "\n"
+                    + "\n");
         } catch (IOException e) {
             throw new ManagerSaveException();
         }
     }
 
-    private <T extends Task> void addTasksToFile(BufferedWriter bufferedWriter, T task)
+    private <T extends Task> void addTasksToFile(BufferedWriter bufferedWriter, Collection<T> tasks)
             throws IOException {
-        bufferedWriter.write(task.toString());
+        for (T task : tasks) {
+            bufferedWriter.write(task.toString());
+        }
+    }
+
+    public static String historyToString(HistoryManager historyManager) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Task record : historyManager.getHistory()) {
+            stringBuilder.append(record.getId()).append(",");
+        }
+        return stringBuilder.toString();
     }
 
     @Override
     public void addTask(Task task) {
         super.addTask(task);
-        save(task);
+        save();
     }
 
     @Override
     public void addEpic(Epic epic) {
         super.addEpic(epic);
-        save(epic);
+        save();
     }
 
     @Override
     public void addSubTask(Subtask subtask) {
         super.addSubTask(subtask);
-        save(subtask);
+        save();
     }
-    // 1. Сделать что-то с получением epic id [DONE]
-    // 2. Логика с добавлением history
-    // 3. Убрать дублирование
 }
