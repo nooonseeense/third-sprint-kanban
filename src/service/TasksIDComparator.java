976bd424700e169023324427318655ace37f75eb
nooonseeense <egor-bolt12@yandex.ru>
2022-09-17 10:24:52 +0300

    feat: Добавить новый класс для сортировки по ID

---
+++ src/service/TasksIDComparator.java
@@ -0,0 +1,12 @@
+package service;
+
+import tasks.Task;
+import java.util.Comparator;
+
+public class TasksIDComparator implements Comparator<Task> {
+
+    @Override
+    public int compare(Task o1, Task o2) {
+        return Integer.compare(o1.getId(), o2.getId());
+    }
+}
