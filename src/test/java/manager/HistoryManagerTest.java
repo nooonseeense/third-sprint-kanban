38cf78159cde428b4928c346e5bdbb94ba314fad
nooonseeense <egor-bolt12@yandex.ru>
2022-11-19 09:47:05 +0300

    feat: новый класс с тестами

---
+++ src/test/java/manager/HistoryManagerTest.java
@@ -0,0 +1,18 @@
+package manager;
+
+import org.junit.jupiter.api.Test;
+
+class HistoryManagerTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getHistory() {
+    }
+}
