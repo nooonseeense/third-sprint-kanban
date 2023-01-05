0ce4014ba7f730f779688faadf4dddadd7ceb295
nooonseeense <egor-bolt12@yandex.ru>
2023-01-05 09:31:42 +0300

    feat: убрать switch case

--- src/main/java/service/TaskAdapter.java
+++ src/main/java/service/TaskAdapter.java
@@ -4,8 +4,6 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
-import tasks.Epic;
-import tasks.Subtask;
 import tasks.Task;
 
 import java.lang.reflect.Type;
@@ -14,17 +12,6 @@ public class TaskAdapter implements JsonDeserializer<Task> {
     @Override
     public Task deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext)
             throws JsonParseException {
-        String taskType = jsonElement.getAsJsonObject().get("type").getAsString();
-
-        switch (taskType) {
-            case "TASK":
-                return jsonDeserializationContext.deserialize(jsonElement, Task.class);
-            case "EPIC":
-                return jsonDeserializationContext.deserialize(jsonElement, Epic.class);
-            case "SUBTASK":
-                return jsonDeserializationContext.deserialize(jsonElement, Subtask.class);
-            default:
-                throw new IllegalArgumentException();
-        }
+        return jsonDeserializationContext.deserialize(jsonElement, Task.class);
     }
 }
